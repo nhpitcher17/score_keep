6b7074a91a6cc11a2d04dec729b028058e968414
Tom Dunlap <prof_myu@yahoo.com>
2017-05-12 18:02:08 -0400

    added first React components11

---
+++ imports/ui/AddPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AddPlayer extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is where add player would go.</p>
+      </div>
+    );
+  }
+}
--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup( () => {
-  class Person {
+/*  class Person {
     constructor(name = '(you forgot to enter a name, dummy!)', age = "We get it. You don't look a day over 18.") {
       this.name = name;
       this.age = age;
@@ -12,6 +12,26 @@ Meteor.startup( () => {
     }
   }
 
-  let me = new Person();
+  class Employee extends Person {
+    constructor(name, age, title) {
+      super(name, age);
+      this.title = title;
+    }
+    getGreeting() {
+      if (this.title) {
+        return super.getGreeting() + ` And ${this.title}!`;
+      } else {
+        return super.getGreeting();
+      }
+
+    }
+    hasJob() {
+      return !!this.title;
+    }
+  }
+
+  let me = new Programmer('Tom', '25', 'Python');
   console.log(me.getGreeting());
+*/
+
 });
